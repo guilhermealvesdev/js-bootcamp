284bbf3141f8125b358aa5e461c6a85636db4c11
guilhermealvesdev <guilherme.alves.trash@gmail.com>
2020-03-09 23:45:18 -0300

    Aulas 34 a 38

---
+++ arrays/todo.js
@@ -0,0 +1,5 @@
+var todo = ['Tomar banho', 'escovar os dentes', 'ir pro trabalho', 'ir pra academia', 'dormir'];
+
+console.log(`You have ${todo.length} todos`);
+console.log(`Todo: ${todo[todo.length - 1]}`);
+console.log(`Todo: ${todo[todo.length - 2]}`);
