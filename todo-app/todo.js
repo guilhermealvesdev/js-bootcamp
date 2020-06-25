d8bbb66de43dae67360284a7cc37a4837aa47669
guilhermealvesdev <guilherme.alves.trash@gmail.com>
2020-06-25 20:19:37 -0300

    ajuste em explicação de item da aula 59

--- todo-app/todo.js
+++ todo-app/todo.js
@@ -29,12 +29,38 @@ const filtro = {
 //Função que renderiza todos os itens da array
 const renderizaFiltro = function(tarefas, filtro){
 
-    //Itens filtrados
+    /*
+        Cria uma nova array (o filter cria) que retorna todos os itens da array TAREFAS (que veio no parâmetro)
+        para armazenar apenas os que estiverem de acordo com o texto digitado no objeto filtro,
+        que por sua vez é alimentado toda vez quando o usuário digita no campo de input
+    */
+
     let filtrados = tarefas.filter(function(item){
         return item.activity.toLowerCase().includes(filtro.texto.toLowerCase());
     });
 
-    //Olha, meu amigo, nem eu entendi isso aqui. Assiste de novo a aula 59, por favor.
+    /*
+        Aqui é elaborado. Mas dá pra entender, juro.
+
+        Pra retornar um valor no filter(), é preciso que alguma condicional seja TRUE.
+
+        No caso, o JS pega a array "filtrados" e atualiza o conteúdo para mostar (return) apenas
+        os valores que estiverem o oposto do filtro.ocultaCompletas (isto é, o checkbox. Se o checkbox
+        estiver TRUE, o primeiro valor do return vira FALSE), OU o valor oposto do completed do item que
+        ele usou para comparar.
+
+        Vamos de novo: ao checar o checkbox, o !filtro.ocultaCompletas estará sempre FALSE (porque checado, está TRUE
+        no DOM). Se deixar assim, nunca retornará nada com o checkbox ativo, certo? Então, precisamos colocar outra condicional,
+        pro JS ir buscar os conteúdos da array principal que estão como completed para comparar.
+
+        Aí, o JS procura no item que ele está comparando se é TRUE or FALSE. Como queremos que apareça
+        apenas as que não estão completadas, a gente pede pra ele buscar o contrário de completed.
+
+        Logo, as tarefas que estiverem na array com valor TRUE (isto é, completas), virarão FALSE no return, logo,
+        não aparecerão se o valor do check estiver FALSE (porque os dois valores darão FALSE no final, e o filter() precisa
+        de um TRUE pra renderizar algo). Se o valor do objeto estiver como FALSE (isto é, incompleto), virará TRUE no
+        return, e aparecerá na lista final (uma tarefa que não foi completada ainda).
+    */
     filtrados = filtrados.filter(function(item){
         return !filtro.ocultaCompletas || !item.completed;
     });
