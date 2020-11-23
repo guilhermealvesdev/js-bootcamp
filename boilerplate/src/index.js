b75163d8c9708540583fc44e1872805f0103f7af
guilhermealvesdev <guilherme.alves.trash@gmail.com>
2020-11-23 17:54:27 -0300

    Aulas 128, 129, e 130

---
+++ boilerplate/src/index.js
@@ -0,0 +1,14 @@
+const name = 'Jen'
+console.log(name)
+
+class Hangman {
+    myMethod() {
+        return 'Testing'
+    }
+}
+
+const hangman = new Hangman();
+
+console.log(hangman.myMethod());
+
+console.log('Teste');
